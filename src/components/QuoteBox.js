48519c67d9ce424e139a72dd762f551e55491613
Andrés Siri <andres.siri@hotmail.com>
2021-03-31 14:57:09 -0300

    Initial quote fetch

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,20 +1,35 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import './QuoteBox.css';
 import {ColorContext} from './../ColorContext.js';
 import './../RandomColor.css';
 
 function QuoteBox() {
   const [colorNumber, setColorNumber] = useContext(ColorContext);
+  const [quoteText, setQuoteText] = useState('');
+  const [author, setAuthor] = useState('');
+  
   function randomNumber() {
     setColorNumber((prev) => prev = Math.floor(Math.random() * (3 + 1)));
   }
+
+  function randomQuote() {
+    fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      setQuoteText(data.content);
+      setAuthor(data.author);   
+    })
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => randomQuote(), []);
   
   return (
     <div id="quote-box" className={`QuoteBox`}>
-        <h1 id="text">Random Quote</h1>
-        <p id="author">Author</p>
-        <button className={`color${colorNumber}`}><a id="tweet-quote">Tweet</a></button>
-        <button id="new-quote" onClick={randomNumber} className={`color${colorNumber}`}>New quote</button>        
+        <h1 id="text">{quoteText}</h1>
+        <p id="author">{author}</p>
+        <button className={`color${colorNumber}`}><a id="tweet-quote" href="youtube.com">Tweet</a></button>
+        <button id="new-quote" onClick={function(event){ randomNumber(); randomQuote()}} className={`color${colorNumber}`}>New quote</button>        
     </div>
   );
 }
