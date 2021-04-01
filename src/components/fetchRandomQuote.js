8fd77305011ab84ae919efb6743082f4ea7aee9d
Andrés Siri <andres.siri@hotmail.com>
2021-04-01 16:04:35 -0300

    done it

---
+++ src/components/fetchRandomQuote.js
@@ -0,0 +1,9 @@
+
+async function fetchRandomQuote() {
+    const response = await fetch('https://api.quotable.io/random');
+    const json = await response.json();
+    
+    return json;
+}
+
+export default fetchRandomQuote;
