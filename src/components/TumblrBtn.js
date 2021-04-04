d800feb092f4f43bcf5440a86e3daa75ad786e1b
Andrés Siri <andres.siri@hotmail.com>
2021-04-03 23:22:17 -0300

    create tumblr button

--- src/components/TumblrBtn.js
+++ src/components/TumblrBtn.js
@@ -1,21 +1,21 @@
 import React, {useState, useContext} from "react";
 import { Context } from "./../Context.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
+import { faTumblrSquare } from "@fortawesome/free-brands-svg-icons";
 import './../styles/icon.css';
 import './../styles/RandomColor.scss';
 
-function TwitterBtn() {
+function TumblrBtn() {
   const {colorNum, quote, auth} = useContext(Context);
   const [colorNumber, setColorNumber] = colorNum;
   const [quoteText, setQuoteText] = quote;  
   const [author, setAuthor] = auth;
 
   return (
-    <a id="tweet-quote" href={`http://twitter.com/intent/tweet?hashtags=quotes&realted=elsirook&text="${quoteText}"%20%20${author}`}>
-      <FontAwesomeIcon icon={faTwitterSquare} class={`icon text-color${colorNumber}`} />
+    <a id="tweet-quote" href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=${author}&content=${quoteText}%20&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`}>
+      <FontAwesomeIcon icon={faTumblrSquare} class={`icon text-color${colorNumber}`} />
     </a>
   );
 }
 
-export default TwitterBtn;
+export default TumblrBtn;
