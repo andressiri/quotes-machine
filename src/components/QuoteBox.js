8a33e91a257c4ba6bd057097891317ec3d18bd10
Andrés Siri <andres.siri@hotmail.com>
2021-04-03 23:22:17 -0300

    create tumblr button

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,10 +1,11 @@
 import React, {useState, useContext} from 'react';
 import {Context} from './../Context.js';
 import './../styles/QuoteBox.css';
-import TwitterBtn from './TwitterBtn.js';
 import './../styles/RandomColor.scss';
-import NewQuoteBtn from './NewQuoteBtn.js';
+import TumblrBtn from './TumblrBtn.js';
+import TwitterBtn from './TwitterBtn.js';
 import AutoBtn from './AutoBtn.js';
+import NewQuoteBtn from './NewQuoteBtn.js';
 
 function QuoteBox() {
   const {colorNum, quote, auth} = useContext(Context);
@@ -16,6 +17,7 @@ function QuoteBox() {
     <div id="quote-box" className={`QuoteBox`}>
       <h1 id="text" className={`text-color${colorNumber}`}>{quoteText}</h1>
       <p id="author" className={`text-color${colorNumber}`}>{author}</p>
+      <TumblrBtn />
       <TwitterBtn />
       <AutoBtn />
       <NewQuoteBtn />
