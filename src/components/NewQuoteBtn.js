5fd79b2b8e7110c79c791b6299660663bf0286b1
Andrés Siri <andres.siri@hotmail.com>
2021-04-04 22:14:59 -0300

    improved color switch with animation

--- src/components/NewQuoteBtn.js
+++ src/components/NewQuoteBtn.js
@@ -6,23 +6,27 @@ import './../styles/RandomColor.scss';
 import './../styles/NewQuoteButton.scss'
 
 function NewQuoteBtn () {
-  const {colorNum, quote, auth, auto, aClass} = useContext(Context);
+  const {colorNum, quote, auth, auto, aClass, aTime, fad} = useContext(Context);
   const [colorNumber, setColorNumber] = colorNum;
   const [quoteText, setQuoteText] = quote;  
   const [author, setAuthor] = auth;
   const [handleAuto, setHandleAuto] = auto;
-  const [autoClass, setAutoClass] = aClass;    
+  const [autoClass, setAutoClass] = aClass;
+  const [fade, setFade] = fad;   
 
   async function HandleNewQuote() {
+    setFade('Out');
     if (handleAuto !== 'Interval is off') {
       setAutoClass('autoBtn btnOff BG-color'); 
       clearInterval(handleAuto);
       setHandleAuto('Interval is off');    
-    };
-    setColorNumber(randomDifNum(colorNumber));
+    };    
     let quoteObj = await fetchRandomQuote();
+    await new Promise(resolve => setTimeout(resolve, 650));
+    setColorNumber(randomDifNum(colorNumber));
     setQuoteText(quoteObj.content);
     setAuthor(quoteObj.author);
+    setFade('In');
   };
 
   useEffect(HandleNewQuote, []);
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {Context} from '../Context.js';
 import RenderAsImage, { ImageAsHtml } from 'react-render-as-image'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,22 +6,25 @@ import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 import { faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 import './../styles/icon.scss';
 import './../styles/RandomColor.scss';
-import './../styles/QuoteBox.scss';
+import './../styles/Quote.scss';
 
 function Quote() {
-    const {colorNum, quote, auth} = useContext(Context);
-    const [colorNumber, setColorNumber] = colorNum;
-    const [quoteText, setQuoteText] = quote;  
-    const [author, setAuthor] = auth;
+  const {colorNum, quote, auth, auto, aClass, aTime, fad} = useContext(Context);
+  const [colorNumber, setColorNumber] = colorNum;
+  const [quoteText, setQuoteText] = quote;  
+  const [author, setAuthor] = auth;
+  const [fade, setFade] = fad;
+  
+  
 
   return (
     <div>
-      <h1 id="text" className={`quote text-color${colorNumber}`}>
-        <FontAwesomeIcon icon={faQuoteLeft} class={`icon text-color${colorNumber}`} />
+      <h1 id="text" className={`quote text-color${colorNumber} fade${fade}`}>
+        <FontAwesomeIcon icon={faQuoteLeft} class={`icon text-color${colorNumber} fade${fade}`} />
         {quoteText}
-        <FontAwesomeIcon icon={faQuoteRight} class={`icon text-color${colorNumber}`} /> 
+        <FontAwesomeIcon icon={faQuoteRight} class={`icon text-color${colorNumber} fade${fade}`} /> 
       </h1>
-      <p id="author" className={`text-color${colorNumber}`}>{`- ${author}`}</p>
+      <p id="author" className={`text-color${colorNumber} fade${fade}`}>{`- ${author}`}</p>
     </div>
   )
 }
