145b0a7dc24fd2b1d63123e3d343d29972ac4f03
Andrés Siri <andres.siri@hotmail.com>
2022-04-08 20:45:14 -0300

    handle searching with enter and refresh matches when conditions are changed

--- Client/src/components/SearchWall/SearchInput.js
+++ Client/src/components/SearchWall/SearchInput.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {Context} from '../../Context.js';
 import useDebounceSearchResults from '../../functions/quoteFunctions/useDebounceSearchResults.js';
 
-function SearchInput ({parentToChild}) {
+function SearchInput () {
   const {colors, forms} = useContext(Context);
   const [colorNumber, setColorNumber] = colors.colorNum;
   const [imgBGColor, setImgBGColor] = colors.imgBG;
@@ -14,12 +14,19 @@ function SearchInput ({parentToChild}) {
     debounceSearchResults(event.target.value);
   };
 
+  const handleEnterSearchInput = (event) => {
+    if (event.key !== 'Enter') return;
+    setSearchValue(event.target.value);
+    debounceSearchResults(event.target.value);
+  };
+
   return (
     <input
       type='text'
       placeholder='Search...'
       className={`NQbtn BG-color${colorNumber} text-color${imgBGColor}`}
       onChange={handleSearchInput}
+      onKeyUp={handleEnterSearchInput}
     />
   );
 };
