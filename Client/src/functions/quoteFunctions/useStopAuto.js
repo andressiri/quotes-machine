b02ecb0bf9b77f292895bd15fdadf619d028c44f
Andrés Siri <andres.siri@hotmail.com>
2022-04-20 21:49:26 -0300

    fix auto quote countdown restart

--- Client/src/functions/quoteFunctions/useStopAuto.js
+++ Client/src/functions/quoteFunctions/useStopAuto.js
@@ -4,8 +4,10 @@ import {Context} from '../../Context.js';
 function useStopAuto() {
   const {auto} = useContext(Context);
   const [handleAuto, setHandleAuto] = auto.hAuto;
+  const [autoTime, setAutoTime] = auto.aTime;
   const [autoClass, setAutoClass] = auto.aClass;
   const [autoTimer, setAutoTimer] = auto.timer;
+  const [autoSeconds, setAutoSeconds] = auto.sec;
   
   const stopAuto = () => {
     if (handleAuto !== 'Interval is off') {
@@ -14,6 +16,7 @@ function useStopAuto() {
       setHandleAuto('Interval is off');
       clearInterval(autoTimer);
       setAutoTimer('Interval is off');
+      setAutoSeconds(autoTime/1000);
     };
   };
   return stopAuto;
